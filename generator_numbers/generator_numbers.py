3b67ee2d29b9460b214e822907eb004ce5e07eee
k2qw <mironbogdan2008@gmail.com>
2026-01-07 14:57:01 +0100

    update generator_numbers
    Fixed a yield float(match) error.

--- generator_numbers/generator_numbers.py
+++ generator_numbers/generator_numbers.py
@@ -2,12 +2,19 @@ import re
 from typing import Callable
 
 def generator_numbers(text: str):
-    patert = r'\s+(-?\d+\.?\d*)\s+'
-    matches = re.finditer(patert, text)
+    pattern = r'\s+(-?\d+\.?\d*)\s+'
+    matches = re.finditer(pattern, text)
     for match in matches:
-        yield float(match)
+        yield float(match.group()) 
+
 def sum_profit(text: str, func: Callable) -> float:
     total = 0.0
     for number in func(text):
         total += number
-    return total
+    return total 
+
+if __name__ == "__main__":
+     text = "10 гномій йшло по лісу, 39 гномів вийшло з лісу й взяли з лісу 34 кілограми дров й 12 літрів води."
+
+result = sum_profit(text, generator_numbers)
+print(f"Сумма чисел в тексте: {result}")
